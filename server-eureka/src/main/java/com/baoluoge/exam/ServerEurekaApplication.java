f8b22952e6b320a3976ce0af38b043156f36e374
PaulZhang <zhangpeng@unisound.com>
2016-10-21 17:38:21 +0800

    add eureka server

---
+++ server-eureka/src/main/java/com/baoluoge/exam/ServerEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.baoluoge.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by PaulZhang on 2016/10/21.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEurekaApplication.class, args);
+    }
+}
