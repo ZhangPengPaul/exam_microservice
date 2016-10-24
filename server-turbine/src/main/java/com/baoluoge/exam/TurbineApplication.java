e31dd688d7eb4f679ff8d0f9d7c20096d8d1e641
PaulZhang <zhangpeng@unisound.com>
2016-10-24 11:32:26 +0800

    add hystrix dashboard

---
+++ server-turbine/src/main/java/com/baoluoge/exam/TurbineApplication.java
@@ -0,0 +1,17 @@
+package com.baoluoge.exam;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * Created by PaulZhang on 2016/10/24.
+ */
+@SpringBootApplication
+@EnableTurbine
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TurbineApplication.class).web(true).run(args);
+    }
+}
