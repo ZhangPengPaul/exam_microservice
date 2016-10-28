abfb2c8f69a4a81ead041c1389719fe358d37daf
PaulZhang <zhangpeng@unisound.com>
2016-10-28 15:40:40 +0800

    add zipkin server

---
+++ server-zipkin/src/main/java/com/baoluoge/exam/ServerZipkinApplication.java
@@ -0,0 +1,19 @@
+package com.baoluoge.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * Created by PaulZhang on 2016/10/28.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinStreamServer
+public class ServerZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerZipkinApplication.class, args);
+    }
+}
